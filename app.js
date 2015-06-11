fd1642954ce9fe2d886414803ec5dabe49e19471
jakeflo88 <jakeflorian.mcf@gmail.com>
2015-06-11 17:39:26 -0300

    Autoscroll setup for one column

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ if (search != null) {
 //listen for twitter response and post data
 socket.on('tweets', function(tweets){
 	
+	//makeshift loop that I can control
 	var i = 0;
 
 	var timer = setInterval(function(){
 
+		//when it comes to the last char of the tweet it stops
 		if (i >= tweets.length) {
 			clearInterval(timer);
 		};
 
+		//grab the chars one at a time from the tweet
 		var charSelect = tweets.substring(i, (i + 1));
 
+		//post it to the column
 		var poster = document.createElement('DIV');
 		var stacker = document.createElement('BR')
 
@@ -27,9 +31,14 @@ socket.on('tweets', function(tweets){
 
 		document.getElementById('a').appendChild(poster);
 
+		//advance the loop
 		i++;
 
+		//autoscroll
+		var tracker = document.getElementById('a');
+		tracker.scrollTop = tracker.scrollHeight;
 
-	}, 500);
+		//loop interval time
+	}, 200);
 
 });
