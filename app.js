51fa1962397f2e5dba7245c5947024f6e527f10d
jakeflo88 <jakeflorian.mcf@gmail.com>
2015-06-11 15:19:25 -0300

    Basic styling, moving on to column logic

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
+//prompt to enter what you want to search
 var search = prompt("Type anything to explore live tweets", "Enter the Matrix");
 
-
+//search data emit
 if (search != null) {
 	socket.emit('test', search);
 }
 
+//listen for twitter response and post data
 socket.on('tweets', function(tweets){
+	
+
+//Basic display
+/*
 	var newTweet = document.createElement('DIV');
 
 	newTweet.appendChild(document.createTextNode(tweets));
 
 	document.getElementById('landing').appendChild(newTweet);
+*/
 });
