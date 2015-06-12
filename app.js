91f2bbf03b63b7240074caa46aef815298dd4461
jakeflo88 <jakeflorian.mcf@gmail.com>
2015-06-12 11:42:38 -0300

    Callback issues, but the streams work a bit better

--- app.js
+++ app.js
@@ -15,6 +15,17 @@ var colActive = 0;
 //listen for twitter response and post data
 socket.on('tweets', function(tweets){
 
+	var colSelect = colActive;
+
+	console.log(tweets);
+
+	if (colActive <= 11) {
+		colActive++;
+	}
+	if (colActive >= 12) {
+		colActive = 0;
+	}
+
 	//check if column is even or odd
 	if (colActive % 2 == 0) {
 	
@@ -26,14 +37,6 @@ socket.on('tweets', function(tweets){
 			//when it comes to the last char of the tweet it stops
 			if (i >= tweets.length) {
 				clearInterval(timer);
-
-					if (colActive <= 11) {
-						colActive++;
-					};
-
-					if (colActive > 11) {
-						colActive = 0;
-					};
 			};
 
 			//grab the chars one at a time from the tweet
@@ -46,13 +49,13 @@ socket.on('tweets', function(tweets){
 			poster.appendChild(document.createTextNode(charSelect));
 			poster.appendChild(stacker);
 
-			var columnUp = document.getElementById(columns[colActive]).appendChild(poster);
+			var columnUp = document.getElementById(columns[colSelect]).appendChild(poster);
 
 			//advance the loop
 			i++;
 
 			//autoscroll
-			var tracker = document.getElementById(columns[colActive]);
+			var tracker = document.getElementById(columns[colSelect]);
 			tracker.scrollTop = tracker.scrollHeight;
 
 			//loop interval time
@@ -69,14 +72,6 @@ socket.on('tweets', function(tweets){
 			//when it comes to the last char of the tweet it stops
 			if (i >= tweets.length) {
 				clearInterval(timer);
-
-					if (colActive < 11) {
-						colActive++;
-					};
-
-					if (colActive >= 11) {
-						colActive = 0;
-					};
 			};
 
 			//grab the chars one at a time from the tweet
@@ -92,15 +87,15 @@ socket.on('tweets', function(tweets){
 			//stacking in opposite way
 			//this may not be working the way I want
 			//it's too hard to tell with the skewed data right now
-			var columnDown = document.getElementById(columns[colActive]);
+			var columnDown = document.getElementById(columns[colSelect]);
 			columnDown.insertBefore(poster2, columnDown.childNodes[0]);
 
 			//advance the loop
 			i++;
 
 			//autoscroll in reverse
-			var tracker2 = document.getElementById(columns[colActive]);
-			tracker2.scrollTop = !tracker.scrollHeight;
+			var tracker2 = document.getElementById(columns[colSelect]);
+			tracker2.scrollTop = !tracker2.scrollHeight;
 
 			//loop interval time
 		}, 250);
